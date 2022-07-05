af64851fda6aba39e6629587c63705f18e7dbf1f
Kish <izakimotho@gmail.com>
2022-07-05 11:16:57 +0300

    Add files via upload

---
+++ school_test.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+SERVICE_NAME=school
+PATH_TO_JAR=/opt/$SERVICE_NAME.jar
+PID_PATH_NAME=/tmp/$SERVICE_NAME"_test"-pid
+LOG_PATH=/opt/$SERVICE_NAME"_test".out
+case $1 in
+start)
+       echo "Starting $SERVICE_NAME ..."
+  if [ ! -f $PID_PATH_NAME ]; then
+       nohup java -jar -Dspring.profiles.active=test $PATH_TO_JAR > $LOG_PATH 2> /dev/null &
+                   echo $! > $PID_PATH_NAME
+       echo "$SERVICE_NAME started ..."
+  else
+       echo "$SERVICE_NAME is already running ..."
+  fi
+;;
+stop)
+  if [ -f $PID_PATH_NAME ]; then
+         PID=$(cat $PID_PATH_NAME);
+         echo "$SERVICE_NAME stopping ..."
+         kill $PID;
+         echo "$SERVICE_NAME stopped ..."
+         rm $PID_PATH_NAME
+  else
+         echo "$SERVICE_NAME is not running ..."
+  fi
+;;
+restart)
+  if [ -f $PID_PATH_NAME ]; then
+      PID=$(cat $PID_PATH_NAME);
+      echo "$SERVICE_NAME stopping ...";
+      kill $PID;
+      echo "$SERVICE_NAME stopped ...";
+      rm $PID_PATH_NAME
+      echo "$SERVICE_NAME starting ..."
+      nohup java -jar -Dspring.profiles.active=test $PATH_TO_JAR > $LOG_PATH 2>  /dev/null &
+      echo $! > $PID_PATH_NAME
+      echo "$SERVICE_NAME started ..."
+  else
+      echo "$SERVICE_NAME is not running ..."
+     fi     ;;
+ esac
